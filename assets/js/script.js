01ff8d8745c777c2534077868310e41e5b756248
Josh <bigreaderjd1996@gmail.com>
2019-03-07 15:55:03 -0600

    added custom searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const apiKey = "ex21C6CKlCuX6rpBRZEXgZyk3zOODA2v"; // very secure
 const apiEndpointSearch = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&";
-const usePlaceholders = false;
+const usePlaceholders = true;
 
-var savedSearches = ["garnet", "amethyst", "pearl", "lapis lazuli", "peridot", "jasper"];
+var savedSearches = ["garnet", "amethyst", "lapis lazuli", "peridot", "bismuth", "malachite", "sugilite", "sardonyx", "blue diamond"];
 
 
 $(document).ready(function() {
@@ -24,22 +24,40 @@ $(document).ready(function() {
 		$('#results').empty().append(placeholders);
 
 	}
+
+
+	$('#add-form').on("submit", function(event) {
+		event.preventDefault(); // stop from submitting
+
+		addSearchButton($('#add-terms').val(), true);
+
+		$('#add-terms').val(""); // clear field
+	})
 	
 
 	// add a button to the list by search term
-	function addSearchButton(search) {
-		$('#search-list').append(
-			$('<button>', {
-				type: "button",
-				class: "list-group-item list-group-item-action",
-				text: search
-			})
-		);
+	function addSearchButton(search, prepend) {
+		if (search.trim() === "") return; // don't add empty buttons
+
+		var button = $('<button>', {
+			type: "button",
+			class: "list-group-item list-group-item-action",
+			text: search
+		});
+
+		if (prepend === true) {
+			$('#search-list').prepend(button.addClass("new"));
+		} else {
+			$('#search-list').append(button);
+		}
 	}
 
 
 	$('#search-list').on("click", "button", function() {
 		searchFor($(this).text());
+
+		$('#search-list button').removeClass("active");
+		$(this).addClass("active");
 	});
 
 	// call API and display results
